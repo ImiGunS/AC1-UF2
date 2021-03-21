cf0143b0895033c803f9f0b6877972a74d302d37
ImiGunS <79653853+ImiGunS@users.noreply.github.com>
2021-03-21 19:56:46 +0100

    Update modifica_estructura.sql

--- PART A/modifica_estructura.sql
+++ PART A/modifica_estructura.sql
@@ -3,13 +3,13 @@ USE db_hotels;
 
 CREATE TABLE clients(
 	client_id 		INT UNSIGNED AUTO_INCREMENT,
-	nom				VARCHAR(15),
-    cognom1			VARCHAR(25),
-    sexe			ENUM('F','M'),
-    data_naixement	DATE,
-    pais_origen_id	TINYINT UNSIGNED,
-    CONSTRAINT PK_client PRIMARY KEY (client_id),
-    CONSTRAINT FK_clients_paisos FOREIGN KEY (pais_origen_id)
+	nom			VARCHAR(15),
+  	cognom1			VARCHAR(25),
+  	sexe			ENUM('F','M'),
+  	data_naixement		DATE,
+  	pais_origen_id		TINYINT UNSIGNED,
+   	CONSTRAINT PK_client PRIMARY KEY (client_id),
+	CONSTRAINT FK_clients_paisos FOREIGN KEY (pais_origen_id)
 		REFERENCES paisos( pais_id)
 );
 
@@ -18,6 +18,6 @@ ALTER TABLE reserves
 	ADD COLUMN client_id INT UNSIGNED AFTER reserva_id;
     
 ALTER TABLE reserves
-    ADD CONSTRAINT fk_reserves_clients FOREIGN KEY (client_id)
+	ADD CONSTRAINT fk_reserves_clients FOREIGN KEY (client_id)
 		REFERENCES clients (client_id);
 
